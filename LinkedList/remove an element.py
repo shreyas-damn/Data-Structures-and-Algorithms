10385aaa96355a656ca28f95bc916b862661a174
Shreyas J <shreyas@Shreyass-MacBook-Air.local>
2025-09-29 19:40:36 +0530

    update

---
+++ LinkedList/remove an element.py
@@ -0,0 +1,56 @@
+class Node:
+    def __init__(self,data,next=None):
+        self.data=data
+        self.next=next
+class Linkedlist:
+    def __init__(self):
+        self.head=None
+    def iae(self,data):
+        new_node=Node(data)
+        if self.head==None:
+            self.head=new_node
+            return
+        itr=self.head
+        while itr.next:
+            itr=itr.next
+        itr.next=new_node
+    def add_vals(self,data_list):
+        for data in data_list:
+            self.iae(data)
+    def get_len(self):
+        count=0
+        itr=self.head
+        while itr:
+            count+=1
+            itr=itr.next
+        return count
+    def remove_at(self,index):
+        if index<0 or index>=self.get_len():
+            raise Exception
+        if index==0:
+            self.head=self.head.next
+            return
+        count=0
+        itr=self.head
+        while itr:
+            if count==index-1:
+                itr.next=itr.next.next
+                break
+            itr=itr.next
+            count+=1
+    def print(self):
+        itr=self.head
+        while itr:
+            print(itr.data,end="-->")
+            itr=itr.next
+        print("None \n")
+ll=Linkedlist()
+ll.add_vals([1,2,3,4,5,6,7,8])
+ll.print()
+ll.remove_at(3)
+ll.print()
+            
+            
+
+
+        
