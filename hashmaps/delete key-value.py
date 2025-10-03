b5c28a502f73fd04b115cb19a8d6bebc43caab10
Shreyas J <supershreyas2006@gmail.com>
2025-10-03 18:03:41 +0530

    update

---
+++ hashmaps/delete key-value.py
@@ -0,0 +1,28 @@
+class HashTable:
+    def __init__(self):
+        self.max=10
+        self.arr=[[]for i in range(self.max)]
+    def get_hash(self,key): 
+        h=0
+        for char in key:
+            h+=ord(char)
+        return h%self.max
+    def __getitem__(self,key):
+        h=self.get_hash(key)
+        return self.arr[h]
+    def __setitem__(self,key,value):
+        h=self.get_hash(key)
+        self.arr[h]=value
+    def __delitem__(self,key):
+        h=self.get_hash(key)
+        self.arr[h]==None
+
+hash=HashTable()
+hash["Inchara"]="Inchy"
+hash["Pranit"]="SmollPP"
+hash["Kaveri"]="Kiwi"
+hash["Shreyas"]="Shreyu"
+del hash["Shreyas"]
+print(hash.arr)       
+    
+        
